aa7485ea2c776b968d83dacc88090f1dadf8f5b4
Ben Graham <bgraham@bgraham.com.au>
2024-06-17 17:11:19 +1000

    0.0.15

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup
+from textwrap import dedent
 
 setup(
-    name='django-filternaut',
-    version='0.0.14',
-    author='Ben Graham',
-    author_email='bgraham@bgraham.com.au',
-    description=('Construct arbitrarily complex Django "Q" filters from flat '
-                 'data, such as query parameters.'),
-    long_description=open('README.rst').read(),
-    url='https://github.com/bennullgraham/filternaut',
-    license='BSD',
-    packages=['filternaut'],
-    install_requires=['six>=1.9.0'],
+    name="django-filternaut",
+    version="0.0.15",
+    author="Ben Graham",
+    author_email="bgraham@bgraham.com.au",
+    description=dedent("""\
+        Construct arbitrarily complex Django "Q" filters from flat data, such
+        as query parameters.
+    """),
+    long_description=open("README.rst").read(),
+    url="https://github.com/bennullgraham/filternaut",
+    license="BSD",
+    packages=["filternaut"],
+    install_requires=["six>=1.9.0", "django>=4.2"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Utilities',
-        'Framework :: Django',
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        "Framework :: Django",
     ],
 )
